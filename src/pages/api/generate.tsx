d532576f1b817571aa7174c96e47c1320210cb4b
harryalbert <harryalbert364@gmail.com>
2023-03-17 11:57:05 -0400

    getting chat working

--- src/pages/api/generate.tsx
+++ src/pages/api/generate.tsx
@@ -5,6 +5,20 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+interface ChatMessage {
+	role: string;
+	content: string;
+}
+
+let messages: ChatMessage[] = [
+	{
+		role: "system",
+		content: "you are dr Suess. Respond like Dr. Suess.",
+	},
+	{role: "user", content: "What is the meaning of life?"},
+	{role: "assistant", content: "give me a response"},
+];
+
 async function getAnswer(req: any, res: any) {
 	if (!configuration.apiKey) {
 		res.status(500).json({
@@ -15,23 +29,17 @@ async function getAnswer(req: any, res: any) {
 		return;
 	}
 
-	const animal = req.body.animal || "";
-	if (animal.trim().length === 0) {
-		res.status(400).json({
-			error: {
-				message: "Please enter a valid animal",
-			},
-		});
-		return;
-	}
-
 	try {
-		const completion = await openai.createCompletion({
-			model: "text-davinci-003",
-			prompt: generatePrompt(animal),
-			temperature: 0.6,
+		// const completion = await openai.createCompletion({
+		// 	model: "text-davinci-003",
+		// 	prompt: generatePrompt(animal),
+		// 	temperature: 0.6,
+		// });
+		const chatGPT = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages,
 		});
-		res.status(200).json({result: completion.data.choices[0].text});
+		res.status(200).json({result: chatGPT.data});
 	} catch (error: any) {
 		// Consider adjusting the error handling logic for your use case
 		if (error.response) {
@@ -46,19 +54,6 @@ async function getAnswer(req: any, res: any) {
 			});
 		}
 	}
-}
-
-function generatePrompt(animal: string): string {
-	const capitalizedAnimal =
-		animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-	return `Suggest three names for an animal that is a superhero.
-  
-  Animal: Cat
-  Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-  Animal: Dog
-  Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-  Animal: ${capitalizedAnimal}
-  Names:`;
 }
 
 export default getAnswer;
